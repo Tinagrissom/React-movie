6817d1b08004abc138b4466292a7e11d9d49c49b
Tina <tinagrissom@yahoo.com>
2021-07-28 19:13:05 -0500

    added loading state to display while movies list is loading

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     // default method is get request
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
@@ -20,6 +22,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   // function fetchMoviesHandler() {
@@ -62,7 +65,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
